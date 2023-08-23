a9457b36604d998d140ff89428aee6614ea37116
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-08-23 10:40:07 +0000

    Hello world!

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/exp/slog"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World"
+	}
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger.Info("hello, world", "user", os.Getenv("USER"))
+}
+
+func main() {
+	fmt.Println("hello")
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
